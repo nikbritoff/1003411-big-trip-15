50adc2328af113d3cb01128ed58110b462f9708d
Nikita Britov <btitov.nd@gmail.com>
2021-08-21 01:19:15 +0300

    Правка ошибок отображения

--- src/view/trip-event-form.js
+++ src/view/trip-event-form.js
@@ -119,7 +119,7 @@ const createEventFormTemplate = (data, resetButtonText) => {
 
       <div class="event__field-group  event__field-group--destination">
         <label class="event__label  event__type-output" for="event-destination-1">
-          Flight
+          ${type}
         </label>
         <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="" list="destination-list-1">
         ${setDestinationList(EVENT_DESTINATION_NAMES)}
