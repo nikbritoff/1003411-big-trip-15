bedcc17313186d88c4836f524a56c541a3afa09f
Nikita Britov <btitov.nd@gmail.com>
2021-09-24 10:15:07 +0300

    Исправление критерия Б31

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,17 +1,17 @@
-const convertTimeFromMiliseconds = (miliseconds) => {
-  const milisecondsInSecond = 1000;
-  const secondsInMinute = 60;
-  const minutesInHour = 60;
-  const hoursInDay = 24;
-  const minuteFromMiliseconds = milisecondsInSecond * secondsInMinute;
-  const hourFromMinutes = minuteFromMiliseconds * minutesInHour;
-  const dayFromHours = hourFromMinutes * hoursInDay;
+const MILISECONDS_IN_SECOND = 1000;
+const SECONDS_IN_MINUTE = 60;
+const MINUTES_IN_HOUR = 60;
+const HOURS_IN_DAY = 24;
+const MINUTE_FROM_MILISECONDS = MILISECONDS_IN_SECOND * SECONDS_IN_MINUTE;
+const HOUR_FROM_MINUTES = MINUTE_FROM_MILISECONDS * MINUTES_IN_HOUR;
+const DAY_FROM_HOURS = HOUR_FROM_MINUTES * HOURS_IN_DAY;
 
-  const calculateDays = Math.floor(miliseconds / dayFromHours);
-  let lostTime = miliseconds - (calculateDays * dayFromHours);
-  const calculateHours = Math.floor((lostTime / hourFromMinutes) % 24);
-  lostTime = miliseconds -  (calculateHours * hourFromMinutes);
-  const calculateMinutes = Math.floor((lostTime / minuteFromMiliseconds) % 60);
+const convertTimeFromMiliseconds = (miliseconds) => {
+  const calculateDays = Math.floor(miliseconds / DAY_FROM_HOURS);
+  let lostTime = miliseconds - (calculateDays * DAY_FROM_HOURS);
+  const calculateHours = Math.floor((lostTime / HOUR_FROM_MINUTES) % HOURS_IN_DAY);
+  lostTime = miliseconds -  (calculateHours * HOUR_FROM_MINUTES);
+  const calculateMinutes = Math.floor((lostTime / MINUTE_FROM_MILISECONDS) % SECONDS_IN_MINUTE);
 
   const days = calculateDays > 0 ? `${String(calculateDays).padStart(2, '0')}D ` : '';
   const spentTime = `${days}${String(calculateHours).padStart(2, '0')}H ${String(calculateMinutes).padStart(2, '0')}M`;
